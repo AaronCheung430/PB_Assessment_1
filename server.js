4a3cdd5dc0aaa72f4ac82f0352dda4c44980dc2b
AaronCheung430 <21007144+AaronCheung430@users.noreply.github.com>
2022-01-16 16:49:09 +0000

    added bootstrap library folder and set up the home page of the html

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', function(req, resp){
-   resp.send('Hello world')
+   resp.send('Hello world again')
 })
 
 app.listen(8090)
+
+
 
