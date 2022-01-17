06a44d08d3faa91751f7d11f4b25f7a1d66e9437
AaronCheung430 <21007144+AaronCheung430@users.noreply.github.com>
2022-01-17 23:38:03 +0000

    created databas.json

--- client/script.js
+++ client/script.js
@@ -1,2 +1,20 @@
 
 
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+
+function Home() {
+    var home = document.getElementById("Home");
+    var Blog = document.getElementById("Blog");
+    home.style.display = "block";
+    Blog.style.display = "none";
+}
+
--- server.js
+++ server.js
@@ -1,9 +1,28 @@
+const fs = require("fs");
 const express = require('express')
 const app = express()
 
-app.get('/', function(req, resp){
-   resp.send('Hello world again')
-})
+
+// app.get('/', function(req, resp){
+//    resp.send('Hello world')
+// })
+
+let database = JSON.parse(fs.readFileSync("database.json"));
+
+app.use(express.json());
+app.use(express.static('client'));
+
+app.get('/blog1', function (req, resp){
+    resp.json(database["Blogs"]["Blog1"]);
+});
+
+
+
+
+
+
+
+
 
 app.listen(8090)
 
